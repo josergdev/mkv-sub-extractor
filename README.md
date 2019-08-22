aff993dfd53d81df404f0ca44d929e85857a067f
joserg <joserodriguezgarcia95@gmail.com>
2019-08-23 00:35:22 +0200

    Intial commit with a very premature version of the tool

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # mkv-sub-extractor
 A small tool to extract subtitles from mkv containers
+
+## Usage
+```
+python3 mkv-sub-extractor.py file.mkv
+```
+
+## Notes
+It is a beta version of the tool, only implemented SubRip/SRT extractions.
+Need mkvmerge and mkvextract from [MKVToolNix](https://mkvtoolnix.download "MKVToolNix") to be installed.
+
---
+++ mkv-sub-extractor.py
@@ -0,0 +1,37 @@
+import subprocess
+import json
+import os
+import sys
+
+def get_sub_info_from_mkv(mkv_file):
+	output = subprocess.run(["mkvmerge", "-i", mkv_file, "-F", "json"], capture_output=True)
+	if output.stderr: print("STDERR", output.stderr)
+	if output.stdout:
+		info = json.loads(output.stdout)
+	return info
+
+def extract_srt_from_mkv(mkv_info):
+	mkv_file = mkv_info["file_name"]
+	name = os.path.splitext(mkv_file)[0]
+
+	tracks = mkv_info["tracks"]
+	sub_tracks = list(filter(lambda track: track["codec"] == "SubRip/SRT", tracks))
+	parsed_sub_tracks = list(map(lambda sub_track: "{}:{}.{}.{}{}_{}.srt".format(sub_track["id"],
+																				   name,
+																			   	   sub_track["properties"]["language"],
+																				   "forced." if sub_track["properties"]["forced_track"] else "",
+																			       sub_track["id"],
+																				   sub_track["properties"]["track_name"]), sub_tracks))
+
+	subprocess.run(["mkvextract", mkv_file, "tracks"] + parsed_sub_tracks)
+
+#	print("Extracting", "{}:{}.{}.srt".format(track_id, name, lang))
+#	subprocess.run(["mkvextract", mkv_file, "tracks", "{}:{}.{}.srt".format(track_id, name, lang)])
+
+if __name__ == "__main__":
+	#mkv_path = "/Volumes/macOS HD/jose/Documents/prueba_mkv/Stranger Things 2x01 MADMAX [WEBRip 720p x264 Dual AC3 5.1 Subs][GrupoHDS].mkv"
+	#extract_srt_from_mkv(mkv_file=mkv_path, track_id=3)
+	mkv_path = sys.argv[1]
+	#print(mkv_path)
+	info = get_sub_info_from_mkv(mkv_file=mkv_path)
+	extract_srt_from_mkv(mkv_info=info)
